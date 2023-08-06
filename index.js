e312a0ca50bf9bb266450fb6a1c50d7d0953d876
Abdurrahmanewu <abdurrahman.ewu.bd@gmail.com>
2023-08-06 11:38:17 +0600

    db connected

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.use(express.json());
 // console.log(process.env.DB_USER);
 // console.log(process.env.DB_PASSWORD);
 
-const uri =
-  "mongodb+srv://La-Riveria:RMC5PCXYDBsSZMlG@cluster0.jdfs3t2.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jdfs3t2.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,22 +25,6 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("La Riveria server is running");
